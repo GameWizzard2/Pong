8caab574ebce17e95bdb4e8f3dc62f3b3d17ad36
GameWizzard2 <98629019+GameWizzard2@users.noreply.github.com>
2024-08-20 14:41:32 -0400

    Refactored code

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+// Draw the game //
+function draw() {
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, width, height);
+
+ctx.fillStyle = "white";
+ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE)
+}
+
 // Creating the window //
 let canvas = document.querySelector("#canvas");
 let ctx = canvas.getContext("2d");
 let width = canvas.width;
 let height = canvas.height;
 
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, width, height);
-
 // Creating the ball //
 const BALL_SIZE = 5;
 let ballPosition = {x: 20, y: 30};
 
-ctx.fillStyle = "white";
-ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE)
+draw();
