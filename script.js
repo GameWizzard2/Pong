3d74dd61a3754925fdc1c4f737a23e7bdbd48fcd
GameWizzard2 <98629019+GameWizzard2@users.noreply.github.com>
2024-08-20 15:00:13 -0400

    got the ball to move

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Draw the game //
+// Draw the game
 function draw() {
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, width, height);
@@ -7,14 +7,32 @@ ctx.fillStyle = "white";
 ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE)
 }
 
-// Creating the window //
+// Update the game
+function update() {
+    ballPosition.x += xSpeed;
+    ballPosition.y += ySpeed;
+}
+
+function gameloop() {
+    draw();
+    update();
+
+    // Call this function after a timeout
+    setTimeout(gameloop, 30)
+}
+
+// Creating the window
 let canvas = document.querySelector("#canvas");
 let ctx = canvas.getContext("2d");
 let width = canvas.width;
 let height = canvas.height;
 
-// Creating the ball //
+// Creating the ball
 const BALL_SIZE = 5;
 let ballPosition = {x: 20, y: 30};
 
-draw();
+// ball settings 
+let xSpeed = 4;
+let ySpeed = 2;
+
+gameloop();
